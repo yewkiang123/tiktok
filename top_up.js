9239cad338510afbdfb77f283c60cfd9023f059a
yewkiang123 <84199754+yewkiang123@users.noreply.github.com>
2023-09-06 15:24:37 +0800

    added delete user data function

--- top_up.js
+++ top_up.js
@@ -101,6 +101,37 @@ async function withdraw_from_wallet(user_id, acc_num, amount){
   }
 }
 
+async function pay_to_user(sender_id,receiver_id,amount){ //user_id sends payment to acc_num
+  try{
+    const senderData = await getWalletData(sender_id); 
+    const receiverData = await getWalletData(receiver_id);
+    if(senderData!= null && receiverData!= null){ //check if both accounts are in db
+      const date = new Date();
+      const credited_wallet = receiver_id;
+      const debited_wallet = sender_id;
+      const senderBal = senderData.balance - amount; //deduct from sender 
+      await db.collection('wallets').doc(sender_id).update({balance: senderBal});
+      const receiverBal = receiverData.balance + amount; //add to receiver
+      await db.collection('wallets').doc(receiver_id).update({balance:receiverBal});
+      //create transaction
+      const transaction_id = Math.random().toString(36).substring(2, 10);
+      await create_transaction(sender_id, credited_wallet, debited_wallet, transaction_id, date, 'User-to-User Payment', amount);
+      console.log(`$${amount} was deducted from ${debited_wallet} to ${credited_wallet} at ${date}`);
+      console.log(`The new balance is ${senderBal}`); //just display new balance of sender
+      console.log(`Receiver balance: ${receiverBal}`); //can delete in future
+      return senderData;
+    }
+    else{ //one or more account invalid
+      console.log("Unable to make payment");
+      return null;
+    }
+  }
+  catch(error){
+    console.log("Error has occured during payment", error);
+    return null;
+  }
+}
+
 async function create_transaction(user_id, credited_wallet, debited_wallet, transaction_id, date, type, amount){
   try {
     const reference_id = Math.random().toString(36).substring(1,15)
@@ -121,22 +152,6 @@ async function create_transaction(user_id, credited_wallet, debited_wallet, tran
   }
 }
 
-async function delete_data(user_id){
-  try {
-    const userRef = db.collection("users").doc(user_id)
-    if(userRef.exists){
-      await db.collection("users").doc(user_id).delete();
-      await db.collection("wallets").doc(user_id).delete();
-      console.log("User accout and wallet deleted successfully")
-    }else{
-      console.log("Wallet does not exist")
-    }
-  } catch (error) {
-    console.log("Error deleting data")
-  }
-}
-
-
 async function execute_transactions(functions){
   for (const func of functions){
     try{
@@ -147,11 +162,35 @@ async function execute_transactions(functions){
   }
 }
 
+async function delete_data(user_id) {
+  try {
+    const userRef = db.collection("users").doc(user_id);
+    const userDoc = await userRef.get(); // Await the result of get()
+
+    if (userDoc.exists) {
+      // Document exists, proceed with deletion
+      await userRef.delete(); // Await the delete operation
+      await db.collection("wallets").doc(user_id).delete(); // Await the delete operation for wallets
+      console.log("User data and wallet removed successfully");
+    } else {
+      console.log("User data does not exist");
+    }
+  } catch (error) {
+    console.error("Unable to delete data:", error);
+  }
+}
+
+
+
+
+
+
 const functionsToExecute = [
   () => top_up_wallet("1693484480174_sn2ful4g", "123", 5),
-  () => withdraw_from_wallet("1693484480174_sn2ful4g", "123", 5)
+  () => withdraw_from_wallet("1693484480174_sn2ful4g", "123", 5),
+  () => pay_to_user("1693484480174_sn2ful4g","1693928955016_5v87ykew",10),
+  () => delete_data("1693983793071_p2jqo76k")
 ];
 
-delete_data("1693981635547_mdzco73h");
-//execute_transactions(functionsToExecute);
 
+execute_transactions(functionsToExecute);
