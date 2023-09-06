a8728eaa33768c8eecc428fcd4dd1c96e4a75a82
Zhi <neozhixuan@gmail.com>
2023-09-06 23:45:34 +0800

    Add methods into classes

--- create.js
+++ create.js
@@ -1,72 +1,83 @@
-const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
-const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
+const {
+  initializeApp,
+  applicationDefault,
+  cert,
+} = require("firebase-admin/app");
+const {
+  getFirestore,
+  Timestamp,
+  FieldValue,
+  Filter,
+} = require("firebase-admin/firestore");
 
-const serviceAccount = require('./secrets/serviceAccount.json');
+const serviceAccount = require("./secrets/serviceAccount.json");
 
 initializeApp({
-  credential: cert(serviceAccount)
+  credential: cert(serviceAccount),
 });
 
 const db = getFirestore();
 
-// Function to generate a unique user ID 
-function generateUniqueUserID() {
-  // Logic to generate a unique ID (e.g., timestamp + random characters)
-  const timestamp = Date.now();
-  const randomChars = Math.random().toString(36).substring(2, 10);
-  return `${timestamp}_${randomChars}`;
-}
-
-// Function to add a new user account
-async function add_new_user_account(accountDetails) {
-  try {
-    // Generate a unique user_id 
-    const user_id = generateUniqueUserID();
-
-    // Create a new user_account object with provided details
-    const user_account = {
-      user_id: user_id,
-      ...accountDetails  //other account details
-    };
-
-    // Save the user_account object to the 'users' collection
-    const userRef = db.collection('users').doc(user_id);
-    await userRef.set(user_account);
-
-    console.log('User account added successfully:', user_account);
-
-    // Call the function to add a new wallet
-    await add_new_wallet(user_id);
-
-  } catch (error) {
-    console.error('Error adding user account:', error);
-    throw error; // Rethrow the error
+class CreateItem {
+  // Function to generate a unique user ID
+  static generateUniqueUserID() {
+    // Logic to generate a unique ID (e.g., timestamp + random characters)
+    const timestamp = Date.now();
+    const randomChars = Math.random().toString(36).substring(2, 10);
+    return `${timestamp}_${randomChars}`;
   }
-}
 
-async function add_new_wallet(user_id){
-  try{
-    const wallet = {
-      user_id: user_id,
-      balance: 0
-    };
+  // Function to add a new user account
+  static async add_new_user_account(accountDetails) {
+    try {
+      // Generate a unique user_id
+      const user_id = this.generateUniqueUserID();
+
+      // Create a new user_account object with provided details
+      const user_account = {
+        user_id: user_id,
+        ...accountDetails, //other account details
+      };
+
+      // Save the user_account object to the 'users' collection
+      const userRef = db.collection("users").doc(user_id);
+      await userRef.set(user_account);
+
+      console.log("User account added successfully:", user_account);
+
+      // Call the function to add a new wallet
+      await this.add_new_wallet(user_id);
+    } catch (error) {
+      console.error("Error adding user account:", error);
+      throw error; // Rethrow the error
+    }
+  }
+
+  static async add_new_wallet(user_id) {
+    try {
+      const wallet = {
+        user_id: user_id,
+        balance: 0,
+      };
       // Save wallet object to 'wallets' collection
-      const walletRef = db.collection('wallets').doc(user_id); // Assuming 'wallets' is the collection name
+      const walletRef = db.collection("wallets").doc(user_id); // Assuming 'wallets' is the collection name
       await walletRef.set(wallet);
 
-      console.log('Wallet added successfully', wallet);
-  } catch(error){
-      console.error('Error adding wallet:', error);
+      console.log("Wallet added successfully", wallet);
+    } catch (error) {
+      console.error("Error adding wallet:", error);
+    }
   }
 }
-
 // Taking data from frontend
 const accountDetailsFromFrontend = {
-  first: 'test',
-  last: 'test',
+  first: "test",
+  last: "test",
   born: 1815,
-  email: 'ada@example.com'// Add more properties as needed
+  email: "ada@example.com", // Add more properties as needed
 };
 
 // Call the function to add a new user account with the provided details
-add_new_user_account(accountDetailsFromFrontend)
+CreateItem.add_new_user_account(accountDetailsFromFrontend);
+
+module.exports = CreateItem;
--- top_up.js
+++ top_up.js
@@ -1,213 +1,245 @@
-const { initializeApp, cert } = require('firebase-admin/app');
-const { getFirestore } = require('firebase-admin/firestore');
+const { initializeApp, cert } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+
 // This is your test secret API key.
-const stripe = require("stripe")('sk_test_51NlgQ0Isn5ijZV1GE7N2HDn4vtGzNq70oBGaMHCJFMAzPjz7Ca63Cne8lYJH3qF1Kp1vMYn1E7pdFv5Q9SjXChRX00Gu22HlVR')
-const serviceAccount = require('./secrets/serviceAccount.json');
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+const serviceAccount = require("./secrets/serviceAccount.json");
 
 //app.listen(4242, () => console.log("Node server listening on port 4242!"));
 
 app.use(express.static("public"));
-app.use(express.json());    
+app.use(express.json());
 
 // Initialize Firebase app and Firestore
 const firebaseApp = initializeApp({
-  credential: cert(serviceAccount)
+  credential: cert(serviceAccount),
 });
 const db = getFirestore(firebaseApp);
 
+class WalletFunctions {
+  static async getWalletData(user_id) {
+    try {
+      const walletRef = db.collection("wallets").doc(user_id);
+      const walletDoc = await walletRef.get();
 
-async function getWalletData(user_id) {
-  try {
-    const walletRef = db.collection('wallets').doc(user_id);
-    const walletDoc = await walletRef.get();
-
-    if (walletDoc.exists) {
-      return walletDoc.data();
-    } else {
-      console.log("Wallet not found");
+      if (walletDoc.exists) {
+        return walletDoc.data();
+      } else {
+        console.log("Wallet not found");
+        return null;
+      }
+    } catch (error) {
+      console.error("Error fetching wallet:", error);
       return null;
     }
-  } catch (error) {
-    console.error("Error fetching wallet:", error);
-    return null;
+  }
+
+  static async top_up_wallet(user_id, acc_num, amount) {
+    try {
+      const walletData = await this.getWalletData(user_id);
+
+      if (walletData !== null) {
+        const date = new Date();
+        const credited_wallet = user_id; // Wallet receiving funds
+        const debited_wallet = acc_num; // Bank account to be deducted from
+        const newBal = walletData.balance + amount;
+        await db.collection("wallets").doc(user_id).update({ balance: newBal });
+        const transaction_id = Math.random().toString(36).substring(2, 10);
+
+        await this.create_transaction(
+          user_id,
+          credited_wallet,
+          debited_wallet,
+          transaction_id,
+          date,
+          "Top up",
+          amount
+        );
+
+        console.log(
+          `$${amount} was topped up from bank account ${debited_wallet} to ${credited_wallet} at ${date}`
+        );
+        console.log("The new balance is", newBal);
+
+        return walletData;
+      } else {
+        console.log("Unable to top up");
+        return null;
+      }
+    } catch (error) {
+      console.error("An error occurred during top-up:", error);
+      return null;
+    }
+  }
+
+  static async withdraw_from_wallet(user_id, acc_num, amount) {
+    try {
+      const walletData = await this.getWalletData(user_id);
+      if (walletData != null) {
+        const date = new Date();
+        const credited_wallet = acc_num; //Withdraw to bank account
+        const debited_wallet = user_id; //withdraw from wallet
+        const newBal = walletData.balance - amount;
+        await db.collection("wallets").doc(user_id).update({ balance: newBal });
+        const transaction_id = Math.random().toString(36).substring(2, 10);
+
+        await this.create_transaction(
+          user_id,
+          credited_wallet,
+          debited_wallet,
+          transaction_id,
+          date,
+          "Withdrawal",
+          amount
+        );
+
+        console.log(
+          `$${amount} was withdrawn from ${debited_wallet} to  bank account ${credited_wallet} at ${date}`
+        );
+        console.log("The new balance is", newBal);
+
+        return walletData;
+      } else {
+        console.log("Unable to withdraw");
+        return null;
+      }
+    } catch (error) {
+      console.error("Error has occured during withdrawal", error);
+      return null;
+    }
+  }
+
+  static async pay_to_user(sender_id, receiver_id, amount) {
+    //user_id sends payment to acc_num
+    try {
+      const senderData = await this.getWalletData(sender_id);
+      const receiverData = await this.getWalletData(receiver_id);
+      if (senderData != null && receiverData != null) {
+        //check if both accounts are in db
+        const date = new Date();
+        const credited_wallet = receiver_id;
+        const debited_wallet = sender_id;
+        const senderBal = senderData.balance - amount; //deduct from sender
+        await db
+          .collection("wallets")
+          .doc(sender_id)
+          .update({ balance: senderBal });
+        const receiverBal = receiverData.balance + amount; //add to receiver
+        await db
+          .collection("wallets")
+          .doc(receiver_id)
+          .update({ balance: receiverBal });
+        //create transaction
+        const transaction_id = Math.random().toString(36).substring(2, 10);
+        await this.create_transaction(
+          sender_id,
+          credited_wallet,
+          debited_wallet,
+          transaction_id,
+          date,
+          "User-to-User Payment",
+          amount
+        );
+        console.log(
+          `$${amount} was deducted from ${debited_wallet} to ${credited_wallet} at ${date}`
+        );
+        console.log(`The new balance is ${senderBal}`); //just display new balance of sender
+        console.log(`Receiver balance: ${receiverBal}`); //can delete in future
+        return senderData;
+      } else {
+        //one or more account invalid
+        console.log("Unable to make payment");
+        return null;
+      }
+    } catch (error) {
+      console.log("Error has occured during payment", error);
+      return null;
+    }
+  }
+
+  static async create_transaction(
+    user_id,
+    credited_wallet,
+    debited_wallet,
+    transaction_id,
+    date,
+    type,
+    amount
+  ) {
+    try {
+      const reference_id = Math.random().toString(36).substring(1, 15);
+      const transaction = {
+        user_id: user_id,
+        credited_wallet: credited_wallet,
+        debited_wallet: debited_wallet,
+        transaction_id: transaction_id,
+        reference_id: reference_id,
+        date: date,
+        type: type,
+        amount: amount,
+      };
+      const transactionRef = db.collection("Transactions").doc(reference_id);
+      await transactionRef.set(transaction);
+    } catch (error) {
+      console.log("Unable to generate transaction");
+    }
+  }
+
+  static async delete_data(user_id) {
+    try {
+      const userRef = db.collection("users").doc(user_id);
+      const userDoc = await userRef.get(); // Await the result of get()
+
+      if (userDoc.exists) {
+        // Document exists, proceed with deletion
+        await userRef.delete(); // Await the delete operation
+        await db.collection("wallets").doc(user_id).delete(); // Await the delete operation for wallets
+        console.log("User data and wallet removed successfully");
+      } else {
+        console.log("User data does not exist");
+      }
+    } catch (error) {
+      console.error("Unable to delete data:", error);
+    }
   }
 }
 
 app.post("/create-payment-intent", async (req, res) => {
   const { amount } = req.body;
   const paymentIntent = await stripe.paymentIntents.create({
-  amount: amount,
-  currency: "sgd",
+    amount: amount,
+    currency: "sgd",
+  });
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
 });
-res.send({
-  clientSecret: paymentIntent.client_secret,
-});
-});
 
-async function top_up_wallet(user_id, acc_num, amount) {
-  try {
-    const walletData = await getWalletData(user_id);
-
-    if (walletData !== null) {
-      const date = new Date();
-      const credited_wallet = user_id; // Wallet receiving funds
-      const debited_wallet = acc_num; // Bank account to be deducted from
-      const newBal = walletData.balance + amount;
-      await db.collection('wallets').doc(user_id).update({ balance: newBal });
-      const transaction_id = Math.random().toString(36).substring(2, 10);
-    
-      await create_transaction(user_id, credited_wallet, debited_wallet, transaction_id, date, 'Top up', amount)
-
-      console.log(`$${amount} was topped up from bank account ${debited_wallet} to ${credited_wallet} at ${date}`);
-      console.log("The new balance is", newBal);
-
-      return walletData;
-    } else {
-      console.log("Unable to top up");
-      return null;
-    }
-  } catch (error) {
-    console.error("An error occurred during top-up:", error);
-    return null;
-  }
-}
-
-async function withdraw_from_wallet(user_id, acc_num, amount){
-  try {
-    const walletData = await getWalletData(user_id);
-    if(walletData != null){
-      const date = new Date();
-      const credited_wallet = acc_num; //Withdraw to bank account
-      const debited_wallet = user_id; //withdraw from wallet
-      const newBal = walletData.balance - amount;
-      await db.collection('wallets').doc(user_id).update({ balance: newBal});
-      const transaction_id = Math.random().toString(36).substring(2, 10);
-
-      await create_transaction(user_id, credited_wallet, debited_wallet, transaction_id, date, 'Withdrawal', amount)
-
-      console.log(`$${amount} was withdrawn from ${debited_wallet} to  bank account ${credited_wallet} at ${date}`);
-      console.log("The new balance is", newBal);
-
-      return walletData;
-    }else{
-      console.log("Unable to withdraw");
-      return null;
-    }
-  } catch (error) {
-    console.error("Error has occured during withdrawal", error);
-    return null;
-  }
-}
-
-async function pay_to_user(sender_id,receiver_id,amount){ //user_id sends payment to acc_num
-  try{
-    const senderData = await getWalletData(sender_id); 
-    const receiverData = await getWalletData(receiver_id);
-    if(senderData!= null && receiverData!= null){ //check if both accounts are in db
-      const date = new Date();
-      const credited_wallet = receiver_id;
-      const debited_wallet = sender_id;
-      const senderBal = senderData.balance - amount; //deduct from sender 
-      await db.collection('wallets').doc(sender_id).update({balance: senderBal});
-      const receiverBal = receiverData.balance + amount; //add to receiver
-      await db.collection('wallets').doc(receiver_id).update({balance:receiverBal});
-      //create transaction
-      const transaction_id = Math.random().toString(36).substring(2, 10);
-      await create_transaction(sender_id, credited_wallet, debited_wallet, transaction_id, date, 'User-to-User Payment', amount);
-      console.log(`$${amount} was deducted from ${debited_wallet} to ${credited_wallet} at ${date}`);
-      console.log(`The new balance is ${senderBal}`); //just display new balance of sender
-      console.log(`Receiver balance: ${receiverBal}`); //can delete in future
-      return senderData;
-    }
-    else{ //one or more account invalid
-      console.log("Unable to make payment");
-      return null;
-    }
-  }
-  catch(error){
-    console.log("Error has occured during payment", error);
-    return null;
-  }
-}
-
-async function create_transaction(user_id, credited_wallet, debited_wallet, transaction_id, date, type, amount){
-  try {
-    const reference_id = Math.random().toString(36).substring(1,15)
-    const transaction = {
-      user_id: user_id,
-      credited_wallet: credited_wallet,
-      debited_wallet: debited_wallet,
-      transaction_id: transaction_id,
-      reference_id: reference_id,
-      date: date,
-      type: type,
-      amount: amount
-    }
-    const transactionRef = db.collection('Transactions').doc(reference_id)
-    await transactionRef.set(transaction)
-  } catch (error) {
-    console.log("Unable to generate transaction")
-  }
-}
-
-async function execute_transactions(functions){
-  for (const func of functions){
-    try{
+async function execute_transactions(functions) {
+  for (const func of functions) {
+    try {
       await func();
-    } catch(error){
+    } catch (error) {
       console.error("An error has occured");
     }
   }
 }
 
-async function delete_data(user_id) {
-  try {
-    const userRef = db.collection("users").doc(user_id);
-    const userDoc = await userRef.get(); // Await the result of get()
-
-    if (userDoc.exists) {
-      // Document exists, proceed with deletion
-      await userRef.delete(); // Await the delete operation
-      await db.collection("wallets").doc(user_id).delete(); // Await the delete operation for wallets
-      console.log("User data and wallet removed successfully");
-    } else {
-      console.log("User data does not exist");
-    }
-  } catch (error) {
-    console.error("Unable to delete data:", error);
-  }
-}
-
-async function delete_data(user_id) {
-  try {
-    const userRef = db.collection("users").doc(user_id);
-    const userDoc = await userRef.get(); // Await the result of get()
-
-    if (userDoc.exists){
-      // Document exists, proceed with deletion
-      await userRef.delete(); // Await the delete operation
-      await db.collection("wallets").doc(user_id).delete(); // Await the delete operation for wallets
-      console.log("User data and wallet removed successfully");
-    } else {
-      console.log("User data does not exist");
-    }
-  } catch (error) {
-    console.error("Unable to delete data:", error);
-  }
-}
-
-
-
-
-
 const functionsToExecute = [
-  () => top_up_wallet("1693484480174_sn2ful4g", "123", 5),
-  () => withdraw_from_wallet("1693484480174_sn2ful4g", "123", 5),
-  () => delete_data("16934812480174_sn2ful4g"),
-  () => pay_to_user("1693484480174_sn2ful4g","1693928955016_5v87ykew",10)
+  () => WalletFunctions.top_up_wallet("1693484480174_sn2ful4g", "123", 5),
+  () =>
+    WalletFunctions.withdraw_from_wallet("1693484480174_sn2ful4g", "123", 5),
+  () => WalletFunctions.delete_data("16934812480174_sn2ful4g"),
+  () =>
+    WalletFunctions.pay_to_user(
+      "1693484480174_sn2ful4g",
+      "1693928955016_5v87ykew",
+      10
+    ),
 ];
-
 
 execute_transactions(functionsToExecute);
