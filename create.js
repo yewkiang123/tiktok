569c8776b7ea8651a05620c6bad5fa72fd134799
yewkiang123 <84199754+yewkiang123@users.noreply.github.com>
2023-09-07 16:50:37 +0800

    added method to check if userid is used

--- create.js
+++ create.js
@@ -19,6 +19,10 @@ initializeApp({
 const db = getFirestore();
 
 class CreateItem {
+
+  constructor(db) {
+    this.db = db;
+  }
   // Function to generate a unique user ID
   static generateUniqueUserID() {
     // Logic to generate a unique ID (e.g., timestamp + random characters)
@@ -26,6 +30,17 @@ class CreateItem {
     const randomChars = Math.random().toString(36).substring(2, 10);
     return `${timestamp}_${randomChars}`;
   }
+
+  static async isUsernameTaken(username) {
+    try {
+      const collectionRef = db.collection("users");
+      const query = await collectionRef.where('username', '==', username).get(); // Execute the query
+      return !query.empty; // Return true if username exists
+    } catch (error) {
+      console.log("Error checking username availability", error);
+    }
+  }
+  
 
   // Function to add a new user account
   static async add_new_user_account(accountDetails) {
@@ -39,19 +54,23 @@ class CreateItem {
         ...accountDetails, //other account details
       };
 
-      // Save the user_account object to the 'users' collection
-      const userRef = db.collection("users").doc(user_id);
-      await userRef.set(user_account);
-
-      console.log("User account added successfully:", user_account);
-
+      if (await CreateItem.isUsernameTaken(accountDetailsFromFrontend.username)) {
+        console.log("Username is already taken");
+      } else {
+        // Save the user_account object to the 'users' collection
+        const userRef = db.collection("users").doc(user_id);
+        await userRef.set(user_account);
+        await this.add_new_wallet(user_id);
+        console.log("User account added successfully:", user_account);
+      }
       // Call the function to add a new wallet
-      await this.add_new_wallet(user_id);
+
     } catch (error) {
       console.error("Error adding user account:", error);
       throw error; // Rethrow the error
     }
   }
+
 
   static async add_new_wallet(user_id) {
     try {
@@ -71,10 +90,12 @@ class CreateItem {
 }
 // Taking data from frontend
 const accountDetailsFromFrontend = {
-  first: "test",
-  last: "test",
+  username: "yewkiang1",
+  password: "Password123",
+  first: "Goh",
+  last: "Yew Kiang",
   born: 1815,
-  email: "ada@example.com", // Add more properties as needed
+  email: "yewkiang123@example.com", // Add more properties as needed
 };
 
 // Call the function to add a new user account with the provided details
